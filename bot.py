f499df587d14aafbf426596b8116263d1432e6aa
Michele Palamidessi <m.palamidessi@computergross.it>
2019-12-18 14:28:33 +0100

    - Custom keyboard command - Random photo command

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import telepot
 from telepot import Bot
 from telepot.loop import MessageLoop
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 import time
 
 # Load the file env from default location
@@ -24,7 +25,22 @@ def on_chat_message(msg):
         bot.sendMessage(chat_id, "Hi {}, nice to meet you!".format(msg.get('from').get('first_name')))
     elif '/sayhelloto' in text:
         name = '' if len(parameters) == 0 else parameters[0]
-        bot.sendMessage(chat_id, "Hi {}, nice to meet you!".format(name))
+        bot.sendMessage(chat_id, "Hi *{}*, nice to meet you!".format(name), parse_mode='Markdown')
+    elif '/sendphoto' in text:
+        category = '' if len(parameters) == 0 else parameters[0]
+        bot.sendPhoto(chat_id, 'https://source.unsplash.com/featured/?{}'.format(category),
+                      caption='Your <b>{}</b> photo is arrived'.format(category),
+                      parse_mode='HTML'
+                      )
+    elif '/keyboard' in text:
+        button1 = KeyboardButton(text="Yes")
+        button2 = KeyboardButton(text="No")
+        button3 = KeyboardButton(text="Don't know")
+        keyboard = ReplyKeyboardMarkup(Keyboard=[
+            [button1, button2, button3]
+        ])
+
+        bot.sendMessage(chat_id, 'This is a custom keyboard', reply_markup=keyboard)
     elif text.startswith('/'):
         bot.sendMessage(chat_id, "I'm sorry {} can't understand!".format(msg.get('from').get('first_name')))
 
