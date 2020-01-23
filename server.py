a42f2fbc5bd7b215790413b9f11faf54716bd03b
Michele Palamidessi <palamidessimichele@gmail.com>
2020-01-23 21:10:16 +0100

    - Added some comments to the server file

--- server.py
+++ server.py
@@ -13,16 +13,19 @@ TOKEN = os.environ['TELEGRAM_TOKEN']
 bot = Bot(TOKEN)
 
 app = Flask(__name__)
+
+# This allow cors on all routes for all origin and methods. This should not be done in a production enviroment
 CORS(app)
+
 
 @app.route('/highscore/<int:from_id>/score/<int:score>')
 def highscore(from_id: int, score: int):
-    print(request, request.args)
+    # Retrieve ids from query string
     message_id = request.args.get('message_id')
     chat_id = request.args.get('chat_id')
-    print(chat_id, from_id, message_id)
     bot.setGameScore(from_id, score, (chat_id, message_id))
     return 'ok'
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
